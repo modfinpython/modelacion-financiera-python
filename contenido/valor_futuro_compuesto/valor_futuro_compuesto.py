ce38b8df62048ec7c6eab38a7df05c88136c3a09
modfinpython <modfinpython@gmail.com>
2023-08-14 13:35:15 -0600

    valor_futuro_compuesto

---
+++ contenido/valor_futuro_compuesto/valor_futuro_compuesto.py
@@ -0,0 +1,3 @@
+def valor_futuro_compuesto(tasa_interes, dias_anio, dias_inversion, dias_capitalizacion, valor_presente):
+    factor_interes_compuesto = (1 + tasa_interes * (dias_capitalizacion / dias_anio)) ** (dias_inversion / dias_capitalizacion)
+    return valor_presente * factor_interes_compuesto
