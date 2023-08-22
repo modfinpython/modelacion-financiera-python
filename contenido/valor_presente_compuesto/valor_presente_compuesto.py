4358726c2b200957ca98aa2e5a6ad29120e1dd73
modfinpython <modfinpython@gmail.com>
2023-08-22 09:25:15 -0600

    Valor presente compuesto

---
+++ contenido/valor_presente_compuesto/valor_presente_compuesto.py
@@ -0,0 +1,4 @@
+def valor_presente_compuesto(tasa_interes, dias_anio, dias_inversion, dias_capitalizacion, valor_futuro):
+    factor_decremento = 1/((1+tasa_interes*dias_capitalizacion /
+                           dias_anio)**(dias_inversion/dias_capitalizacion))
+    return valor_futuro*factor_decremento
