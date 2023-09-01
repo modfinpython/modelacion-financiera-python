b7ceda0d71b21c630e9fb40f82dc86d8196f1a8a
modfinpython <modfinpython@gmail.com>
2023-08-31 19:45:22 -0600

    Se agrega valor presente continuo y valor futuro continuo

---
+++ contenido/valor_presente_continuo/valor_presente_continuo.py
@@ -0,0 +1,6 @@
+import math
+
+
+def valor_presente_continuo(tasa_continua, dias_anio, dias_inversion, valor_futuro):
+    factor_decremento_continuo = math.exp(-tasa_continua * dias_inversion/dias_anio)
+    return valor_futuro*factor_decremento_continuo
